36f37593ab29febfff73738cea7506d06650a89f
spkesDE <1682314+spkesde@users.noreply.github.com>
2023-03-10 21:55:15 +0100

    code cleanup app.ts

--- app.ts
+++ app.ts
@@ -1,46 +1,17 @@
-import Homey from 'homey';
+import Homey, {ApiApp} from 'homey';
 import TasmotaVersionChecker from "./tasmotaVersionChecker";
 
 export default class TasmotaMqttApp extends Homey.App {
-    private MQTTClient: any;
+    private MQTTClient!: ApiApp;
     clientAvailable: boolean = false;
     private applicationVersion: any;
     debug: boolean = false;
     private applicationName: string = 'TasmotaMqtt';
     private versionChecker!: TasmotaVersionChecker;
     private topics: string[] = [];
-    private drivers: { [p: string]: any } = {};
+    private drivers: any = {};
     private lastMqttMessage: any;
-    private checkConnection!: NodeJS.Timeout;
-
-    connectMqttClient() {
-        this.MQTTClient = this.homey.api.getApiApp('nl.scanno.mqtt');
-        this.MQTTClient
-            .on('install', () => this.register())
-            .on('uninstall', () => this.unregister())
-            .on('realtime', (topic: any, message: any) => {
-                this.onMessage(topic, message);
-            });
-        try {
-            this.MQTTClient.getInstalled()
-                .then((installed: any) => {
-                    this.clientAvailable = installed;
-                    this.log(`MQTT client status: ${this.clientAvailable}`);
-                    if (installed) {
-                        this.register();
-                        this.homey.apps.getVersion(this.MQTTClient).then((version) => {
-                            this.log(`MQTT client installed, version: ${version}`);
-                        });
-                    }
-                }).catch((error: Error) => {
-                this.log(`MQTT client app error: ${error}`);
-            });
-        } catch (error) {
-            this.log(`MQTT client app error: ${error}`);
-        }
-
-    }
-
+    private checkConnectionInterval!: NodeJS.Timeout;
 
     async onInit() {
         try {
@@ -62,30 +33,21 @@ export default class TasmotaMqttApp extends Homey.App {
         //Setup MQTT client
         this.topics = ["stat", "tele"];
         this.drivers = this.homey.drivers.getDrivers();
-        this.lastMqttMessage = undefined;
-        this.clientAvailable = false;
-        this.connectMqttClient();
+        let result = await this.connectMqttClient().catch(this.error);
+        if (!result) {
+            this.error("Failed to connect to MQTT.");
+        }
+        //Logging for startup
         this.log(`${this.applicationName} is running. Version: ${this.applicationVersion}, debug: ${this.debug}`);
         if (this.debug)
             this.log(`All files in app: ${this.versionChecker.getAllFiles("/userdata", [])}`);
-        this.checkConnection = this.homey.setInterval(() => {
-            try {
-                if ((this.lastMqttMessage !== undefined) && (Date.now() - this.lastMqttMessage > 10 * 60 * 1000)) {
-                    this.log(`MQTT connection timeout. Resetting connection`);
-                    this.lastMqttMessage = undefined;
-                    this.connectMqttClient();
-                }
-            } catch (error) {
-                if (this.debug)
-                    throw(error);
-                else
-                    this.log(`${this.constructor.name} checkDevices error: ${error}`);
-            }
-        }, 60000);
+
+        //Starting interval to check connection of the MQTT client
+        this.checkConnection();
 
     }
 
-    onMessage(topic: string, message: string) {
+    public onMessage(topic: string, message: string) {
         let topicParts = topic.split('/');
         if (topicParts.length > 1) {
             this.lastMqttMessage = Date.now();
@@ -97,21 +59,18 @@ export default class TasmotaMqttApp extends Homey.App {
         }
     }
 
-    subscribeTopic(topicName: string) {
-        if (!this.clientAvailable)
-            return;
-        return this.MQTTClient.post('subscribe', {topic: topicName}, (error: Error) => {
-            if (error) {
+    public subscribeToTopic(topicName: string) {
+        if (!this.clientAvailable) return;
+        this.MQTTClient.post('subscribe', {topic: topicName})
+            .then(() =>
+                this.log(`Successfully subscribed to topic: ${topicName}`)
+            )
+            .catch((error: Error) =>
                 this.log(`Can not subscribe to topic ${topicName}, error: ${error}`)
-            } else {
-                this.log(`Successfully subscribed to topic: ${topicName}`);
-            }
-        }).catch((error: Error) => {
-            this.log(`Error while subscribing to ${topicName}. ${error}`);
-        });
+            );
     }
 
-    sendMessage(topic: string, payload: string) {
+    public sendMessage(topic: string, payload: string) {
         this.log(`sendMessage: ${topic} <= ${payload}`);
         if (!this.clientAvailable)
             return;
@@ -120,22 +79,55 @@ export default class TasmotaMqttApp extends Homey.App {
             retain: false,
             mqttTopic: topic,
             mqttMessage: payload
-        }, (error: Error) => {
-            if (error)
-                this.log(`Error sending ${topic} <= "${payload}"`);
         }).catch((error: Error) => {
             this.log(`Error while sending ${topic} <= "${payload}". ${error}`);
         });
     }
 
-    register() {
-        this.clientAvailable = true;
+    private checkConnection() {
+        this.checkConnectionInterval = this.homey.setInterval(async () => {
+            if ((this.lastMqttMessage !== undefined && (Date.now() - this.lastMqttMessage > 10 * 60 * 1000)) || !this.clientAvailable) {
+                this.log(`MQTT connection timeout. Resetting connection`);
+                this.lastMqttMessage = undefined;
+                await this.connectMqttClient();
+            }
+        }, 1000 * 60);
+    }
+
+    private async connectMqttClient(): Promise<boolean> {
+        return new Promise(async (resolve) => {
+            this.MQTTClient = this.homey.api.getApiApp('nl.scanno.mqtt') as ApiApp;
+            if (this.MQTTClient == undefined) {
+                this.log("MQTT client not found");
+                resolve(false);
+            }
+            if (!await this.MQTTClient.getInstalled().catch(this.error)) {
+                this.log("MQTT not installed");
+                await this.homey.notifications.createNotification({
+                    excerpt: "MQTT not installed. Please install MQTT Client to use Tasmota MQTT. (https://homey.app/a/nl.scanno.mqtt/)",
+                })
+                resolve(false);
+            }
+            this.MQTTClient
+                .on('install', () => this.register())
+                .on('uninstall', () => this.unregister())
+                .on('realtime', (topic: any, message: any) => {
+                    this.onMessage(topic, message);
+                });
+            this.clientAvailable = true;
+            this.log(`MQTT client status: ${await this.MQTTClient.getInstalled().catch(this.error)} - ${await this.MQTTClient.getVersion().catch(this.error)}`);
+            this.register();
+            resolve(true);
+        });
+    }
+
+    private register() {
         // Subscribing to system topic to check if connection still alive (update ~10 second for mosquitto)
-        this.subscribeTopic("$SYS/broker/uptime");
+        this.subscribeToTopic("$SYS/broker/uptime");
         this.lastMqttMessage = Date.now();
         for (let topic in this.topics) {
-            this.subscribeTopic(this.topics[topic] + "/#");
-            this.subscribeTopic("+/" + this.topics[topic] + "/#");
+            this.subscribeToTopic(this.topics[topic] + "/#");
+            this.subscribeToTopic("+/" + this.topics[topic] + "/#");
         }
         let now = Date.now();
         Object.keys(this.drivers).forEach((driverId) => {
@@ -146,7 +138,7 @@ export default class TasmotaMqttApp extends Homey.App {
         });
     }
 
-    unregister() {
+    private unregister() {
         this.clientAvailable = false;
         this.lastMqttMessage = undefined;
         this.log(`${this.constructor.name} unregister called`);
